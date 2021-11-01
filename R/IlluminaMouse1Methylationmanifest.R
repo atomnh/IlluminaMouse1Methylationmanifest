4549a4c24b594230384035dd7665686c9adb41fc
CCGM <ccgmlihua@CCGMs-iMac.local>
2021-11-02 07:18:43 +0800

    according to the epic annotation scripts

---
+++ R/IlluminaMouse1Methylationmanifest.R
@@ -0,0 +1,13 @@
+#' Data of manifest for illumina mouse methylation beadchip on 02/11/2021
+#' create the manifest in reference of IlluminaHumanMethylationEPICmanifest. 
+#'
+#' @format A DataFrame with 5 sub DataFrames:
+#' \describe{
+#'   \item{`type I probes`}{Number of type I probes: 142262 }
+#'   \item{`type II probes`}{Number of type II probes: 724574} 
+#'   \item{`control probes`}{Number of control probes: 635 }
+#'   \item{`SNP type I probes`}{Number of SNP type I probes: 21}
+#'   \item{`SNP type II probes`}{Number of SNP type II probes: 38}
+#' }
+#' @source \url{https://support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/mouse-methylation/infinium-mouse-methylation-manifest-file-csv.zip}
+"IlluminaMouse1Methylationmanifest"
